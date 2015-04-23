a8d45848a106fd68ade5767d86b8b25642210f26
d-shiraishi <d_shiraishi@waku-2.com>
2015-04-23 21:35:57 +0900

    Back up PostgreSQL by cron.

--- docker-cron/data_backup.sh
+++ docker-cron/data_backup.sh
@@ -1,5 +1,11 @@
-#!/bin/bash
+#!/bin/bash -e
+
 
 DATE=`date +%Y%m%d`
-echo "Hello World! DATE=${DATE}"
+# DATE=`date +%Y%m%d-%H:%M`
+
+echo "Start backup."
+echo "PGPASSWORD=**** pg_dumpall -h ${DB_1_PORT_5432_TCP_ADDR} -p ${DB_1_PORT_5432_TCP_PORT} -U ${DB_1_ENV_POSTGRES_USER} > /backups/dumpall-${DATE}.sql"
+PGPASSWORD=${DB_1_ENV_POSTGRES_PASSWORD} pg_dumpall -h ${DB_1_PORT_5432_TCP_ADDR} -p ${DB_1_PORT_5432_TCP_PORT} -U ${DB_1_ENV_POSTGRES_USER} > /backups/dumpall-${DATE}.sql
+echo "Finish backup."
 
