ab16ccfe6acd2a9ca43830a1f628b1315c6382bf
dsk0701 <d.dubandtribal@gmail.com>
2015-05-07 21:38:26 +0900

    Update bachup.sh.

--- backup.sh
+++ backup.sh
@@ -1,11 +1,10 @@
 #! /bin/bash -e
 
 CONTAINER_BACKUP_SRC_DIR=/var/lib/postgresql/data
-CONTAINER_BACKUP_DST_DIR=/backup
+CONTAINER_BACKUP_DST_DIR=/backups
 BACKUP_FILE_NAME=`date +"%Y%m%d_%I%M"`
 HOST_BACKUP_DIR=`pwd`
 
-# SONAR_CONTAINER_ID=`docker ps | grep sonar | awk '{print $1}'`
 STORAGE_CONTAINER_ID=`docker ps | grep storage | awk '{print $1}'`
 
 # コンテナを停止します。
@@ -19,13 +18,15 @@ docker run --rm --volumes-from ${STORAGE_CONTAINER_ID} -v ${HOST_BACKUP_DIR}:${C
 COUNT=0
 ls -1t ${HOST_BACKUP_DIR}/backup-* | while read LINE
 do
+    echo "LINE:${LINE}"
     if [ $COUNT -gt 3 ]; then
-        echo "Remove unnecessary backup file."
+        echo "Remove old backup file:${LINE}"
         rm -f ${LINE}
     fi
-    COUNT=$(expr $COUNT + 1)
+    COUNT=$((COUNT + 1))
 done
 
-# コンテナを再開します。
-docker-compose up --no-recreate -d
+# コンテナを開始します。
+echo "Starting containers..."
+docker-compose start
 
